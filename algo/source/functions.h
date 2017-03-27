1d966c3e7b42f22ea324947117666d8d16b43f02
Pruvost Geoffrey <geoffrey.pruvost62@gmail.com>
2017-03-27 21:23:45 +0200

    use makefile

--- algo/source/functions.h
+++ algo/source/functions.h
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <zconf.h>
+#include <unistd.h>
 
 /* Initialisation of variables & test program arguments */
 int initParameters(int argc, char * argv[], int * sizeOfSolution, int * numberOfEvaluation);
