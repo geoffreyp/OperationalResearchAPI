ccfd2c7497f7010cf41747632e15eedbb866521d
Geoffrey <geoffrey.pruvost@hotmail.com>
2017-06-03 17:32:00 +0200

    refactoring the code

--- grpcServices/HillClimberFirstImprovement.cpp
+++ grpcServices/HillClimberFirstImprovement.cpp
@@ -50,30 +50,23 @@ Status HillClimberFirstImprovement::SendFitness(ServerContext* context, const Fi
 
 
     /*
-     * save data in mongodb if it is a better solution (Hill Climber First Improvement)
+     * save data in mongodb if it is a better solution (Hill Climber First Improvement with minimisation)
      */
     // get the transaction
-    auto filterFitnessToSearch = bsoncxx::builder::stream::document{} ;
-    bsoncxx::oid myoid(request->id());
-    filterFitnessToSearch << "_id" << myoid;
+    auto filterTransactionToSearch = bsoncxx::builder::stream::document{} ;
+    filterTransactionToSearch << "_id" << bsoncxx::oid(request->id());
 
+    mongocxx::stdx::optional<bsoncxx::document::value> view = transac_coll.find_one(filterTransactionToSearch.view());
 
-    mongocxx::stdx::optional<bsoncxx::document::value> view = transac_coll.find_one(filterFitnessToSearch.view());
     if(view) {
         // get the best fitness of the transaction
-        bsoncxx::document::view vtmp = *view;
-        bsoncxx::document::element eltBestFitnessId= vtmp["best_fitness_id"];
-        std::string idBestFitness = eltBestFitnessId.get_oid().value.to_string();
+        bsoncxx::document::view viewFitnessToSearch = *view;
+        bsoncxx::document::element eltBestFitnessId= viewFitnessToSearch["best_fitness_id"];
 
-        bsoncxx::document::element eltTransacId= vtmp["_id"];
-        std::string transacIdInColl = eltTransacId.get_oid().value.to_string();
-      //  std::cout << "transacIdInColl:::::: " << transacIdInColl << std::endl;
-       // std::cout << "transacId received::: " << request->id() << std::endl << std::endl;
+        auto filterFitnessToSearch = bsoncxx::builder::stream::document{} ;
+        filterFitnessToSearch << "_id" << eltBestFitnessId.get_oid();
 
-        auto filter = bsoncxx::builder::stream::document{} ;
-        filter << "_id" << eltBestFitnessId.get_oid();
-
-        auto docFitnessFindOne = fitness_coll.find_one(filter.view());
+        auto docFitnessFindOne = fitness_coll.find_one(filterFitnessToSearch.view());
         auto bestFitness = docFitnessFindOne.value().view()["fitness"].get_double().value;
 
         auto bestSolution = docFitnessFindOne.value().view()["solution"].get_utf8().value.to_string();
@@ -82,8 +75,6 @@ Status HillClimberFirstImprovement::SendFitness(ServerContext* context, const Fi
         // If the new fitness is better (Case of minimization) than the actual best fitness, then we replace the solution
         if(bestFitness > request->fitness()){
 
-           // std::cout << "best fitness : " << bestFitness << std::endl;
-           // std::cout << "fitness recu : " << request->fitness() << std::endl;
 
             bsoncxx::builder::stream::document documentFitnessToInsert{};
             documentFitnessToInsert << "transaction_id" << request->id();
@@ -92,7 +83,7 @@ Status HillClimberFirstImprovement::SendFitness(ServerContext* context, const Fi
 
             bestSolution = request->solution();
             bsoncxx::types::value  newBestFitnessId = fitness_coll.insert_one(documentFitnessToInsert.view())->inserted_id();
-            //std::cout << "id new best fitness : " << newBestFitnessId.get_oid().value.to_string() << std::endl << std::endl;
+
             transac_coll.update_one(
                     bsoncxx::builder::stream::document{} << "_id" << bsoncxx::oid(request->id()) <<   bsoncxx::builder::stream::finalize,
                     bsoncxx::builder::stream::document{} << "$set" <<  bsoncxx::builder::stream::open_document <<
