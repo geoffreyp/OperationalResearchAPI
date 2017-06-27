68da8a3cbdf404934d3eede48f479df291023ee2
Geoffrey <geoffrey.pruvost@hotmail.com>
2017-06-27 17:34:26 +0200

    register the hello world service

--- server.cpp
+++ server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include "grpcServices/HillClimberFirstImprovement.h"
+#include "grpcServices/HelloWorld.h"
 
 
 
@@ -11,10 +12,12 @@
 void RunServer() {
     std::string server_address("0.0.0.0:50051");
     HillClimberFirstImprovement service;
+    HelloWorld helloWorldService;
 
     ServerBuilder builder;
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
+    builder.RegisterService(&helloWorldService);
 
     std::unique_ptr<Server> server(builder.BuildAndStart());
     std::cout << "Server listening on " << server_address << std::endl;
